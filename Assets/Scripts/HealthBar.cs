76a1a61c3bd8e61914d119721cf41d44dae16d3d
Igor Konyakhin <nns2009@yandex.ru>
2020-08-08 11:05:01 +0300

    Health Bar UI and camera Audio Source added

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBar : MonoBehaviour
+{
+    public Damageable Damageable;
+    public TextMeshProUGUI HealthText;
+    public Image HealthRemainingBar;
+
+    public float AdjustmentSpeed;
+
+    private float lastShowedHealth = -1;
+
+    public void Update()
+    {
+        if (lastShowedHealth < 0)
+        {
+            lastShowedHealth = Damageable.Health;
+        }
+        var targetHealth = Damageable.Health;
+        lastShowedHealth = Mathf.Lerp(lastShowedHealth, targetHealth, AdjustmentSpeed);
+
+        HealthText.text = Damageable.Health + " / " + Damageable.MaxHealth;
+        HealthRemainingBar.transform.localScale = new Vector3(lastShowedHealth / Damageable.MaxHealth, 1, 1);
+    }
+}
