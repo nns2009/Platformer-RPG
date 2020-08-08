834c400c85c9e0b174cd45e5ebeb5744022bb21f
Igor Konyakhin <nns2009@yandex.ru>
2020-08-08 16:12:16 +0300

    Created LevelEnder prefab and script

---
+++ Assets/Scripts/LevelEnder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnder : MonoBehaviour
+{
+    public GameObject ToActivate;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<HeroController>() != null)
+        {
+            Debug.Log("Changing levels!");
+
+            if (ToActivate != null)
+            {
+                ToActivate.SetActive(true);
+            }
+
+            if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCount)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+        }
+    }
+}
